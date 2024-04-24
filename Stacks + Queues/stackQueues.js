48c0c8417f041b0da4227da6c7698098bcfd8c50
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-04-24 19:43:11 +0530

    queues added

--- Stacks + Queues/stackQueues.js
+++ Stacks + Queues/stackQueues.js
@@ -69,5 +69,44 @@ myStack2.push("discord");
 myStack2.pop();
 // myStack2.pop();
 // myStack2.pop();
-console.log(myStack2.peek());
-console.log(myStack2);
+// console.log(myStack2.peek());
+// console.log(myStack2);
+
+// ----NEXT----
+class Queues {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue() {
+    if (!this.first) return null;
+    if (this.first === this.last) this.last = null;
+    const holdingPointer = this.first;
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+}
+const myQueue = new Queues();
+myQueue.enqueue("google");
+myQueue.enqueue("udemy");
+myQueue.enqueue("discord");
+myQueue.dequeue();
+myQueue.peek();
+console.log(myQueue);
