b1ed58b325b26c4cf351ee4c1b3a63d0ffebf644
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-05-03 19:59:08 +0530

    breadth first serch

--- Trees/trees.js
+++ Trees/trees.js
@@ -50,29 +50,71 @@ class BinarySearchTree {
     let current = this.root;
     let parent = null;
     while (current) {
-      if (value < current.value) {
-        parent = current;
-        current = current.left;
-      } else if (value > current.value) {
-        parent = current;
-        current = current.right;
-      } else if (current.value === value) {
+      if (value === current.value) {
         if (!current.left && !current.right) {
           if (!parent) this.root = null;
           else if (parent.left === current) parent.left = null;
-          else if (parent.right === current) parent.right = null;
-        } else if (!current.left) {
-          if (!parent) this.root = current.right;
-          else if (parent.left === current) parent.left = current.right;
-          else if (parent.right === current) parent.right = current.right;
+          else parent.right = null;
+
+          return true;
         } else if (!current.right) {
           if (!parent) this.root = current.left;
           else if (parent.left === current) parent.left = current.left;
-          else if (parent.right === current) parent.right = current.left;
+          else parent.right = current.left;
+
+          return true;
+        } else if (!current.left) {
+          if (!parent) this.root = current.right;
+          else if (parent.left === current) parent.left = current.right;
+          else parent.right = current.right;
+
+          return true;
+        } else {
+          let successor = current.right;
+          let successorParent = current;
+          while (successor.left) {
+            successorParent = successor;
+            successor = successor.left;
+          }
+          current.value = successor.value;
+          if (successorParent.left === successor)
+            successorParent.left = successor.right;
+          else successorParent.right = successor.right;
+
+          return true;
         }
+      } else if (value < current.value) {
+        parent = current;
+        current = current.left;
+      } else {
+        parent = current;
+        current = current.right;
       }
-      return this;
     }
+    return false;
+  }
+
+  breadthFirstSearch() {
+    if (!this.root) return false;
+    let queue = [];
+    let list = [];
+    queue.push(this.root);
+    while (queue.length) {
+      let current = queue.shift();
+      list.push(current.value);
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    return list;
+  }
+
+  breadthFirstSearchRecursive(queue, list) {
+    if (!queue.length) return list;
+    let current = queue.shift();
+    list.push(current.value);
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+    return this.breadthFirstSearchRecursive(queue, list);
   }
 }
 const tree = new BinarySearchTree();
@@ -86,4 +128,6 @@ tree.insert(1);
 // console.log(tree.lookup(9));
 console.log(tree.remove(1));
 // console.log(tree);
-// console.log(JSON.stringify(tree));
+console.log(JSON.stringify(tree));
+console.log(tree.breadthFirstSearch());
+console.log(tree.breadthFirstSearchRecursive([tree.root], []));
